d5c959546a830caa204dc828c84db9b2b986c404
Tamaki Ryotaro <t.ryotaro.soccer.24@gmail.com>
2026-04-16 17:34:39 +0900

    生徒追加のボタンのJS完成

--- php_do/student_add_do.php
+++ php_do/student_add_do.php
@@ -1,4 +1,10 @@
+<!-- http://localhost:8080/amino-acids/admin_student_add.php -->
 <!-- 生徒登録実行処理 -->
-<?php 
+<?php
 require_once __DIR__ . '/../functions/functions.php';
+
+// $course_id = $_POST;
 ?>
+<pre>
+<?php var_dump($_POST); ?>
+</pre>
