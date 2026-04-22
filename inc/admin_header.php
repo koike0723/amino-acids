3bf89d8f6257ca40c7fac6da6e02be4af147c3ad
Emiko Kishimoto <emiko.markz.school@gmail.com>
2026-04-22 15:48:59 +0900

    生徒データ削除機能作成途中

--- inc/admin_header.php
+++ inc/admin_header.php
@@ -1,7 +1,7 @@
 <!-- 管理者側ヘッダー -->
-<?php require_once __DIR__ . '/functions/functions.php'; ?>
-<?php $data = $row['status_id']; 
-$iconbanner = !empty($data) 
+<?php require_once __DIR__ . '/../functions/functions.php'; ?>
+<?php $data = $row['status_id'];
+$iconbanner = !empty($data)
 ?>
 <header class="site-header_admin">
     <div class="header-inner">
