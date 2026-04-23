ab500034484b04f6a053282b5c6eaed4dc712199
Yoshiaki Koike <koike.y44@gmail.com>
2026-04-23 10:48:23 +0900

    feat: 申請関係のファイルを分離+申請内容一覧を表示する関数を作成

--- functions/functions.php
+++ functions/functions.php
@@ -4,3 +4,4 @@ require_once __DIR__ . '/includes/students.php';
 require_once __DIR__ . '/includes/courses.php';
 require_once __DIR__ . '/includes/cc_slots.php';
 require_once __DIR__ . '/includes/cc_bookings.php';
+require_once __DIR__ . '/includes/requests.php';
